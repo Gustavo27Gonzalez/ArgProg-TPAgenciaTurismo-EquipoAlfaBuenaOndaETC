dbe2334fcb7976c6e3f755441ce423a08adb4308
Tomas <tomasandrada1012@gmail.com>
2021-10-24 05:17:04 -0300

    Se corrigieron errores

--- src/main/java/dao/ItinerarioDAO.java
+++ src/main/java/dao/ItinerarioDAO.java
@@ -9,19 +9,32 @@ import jdbc.ConnectionProvider;
 import usuario.Usuario;
 
 public class ItinerarioDAO {
+	private Connection conn;
+	
 
-	public void generarItinerario() throws SQLException {
-		String sql = "INSERT INTO itinerario (id_usuario, compra) VALUES (?, ?)";
-		Connection conn = ConnectionProvider.getConnection();
-
-		for (Usuario usuario : UsuarioDAOImpl.listaUsuarios) {
-			for (Atraccion atraccion : usuario.atraccionesCompradas) {
-				PreparedStatement statement = conn.prepareStatement(sql);
-				statement.setLong(1, usuario.getId());
-				statement.setString(2, atraccion.getNombre());
-				statement.executeUpdate();
+	public void generarItinerario() {
+		try {
+			String sql = "INSERT INTO itinerario (id_usuario, compra) VALUES (?, ?)";
+			Connection conn = ConnectionProvider.getConnection();
+			
+			for (Usuario usuario : UsuarioDAOImpl.listaUsuarios) {
+				for (Atraccion atraccion : usuario.atraccionesCompradas) {
+					PreparedStatement statement = conn.prepareStatement(sql);
+					statement.setLong(1, usuario.getId());
+					statement.setString(2, atraccion.getNombre());
+					statement.executeUpdate();
+				}
+			}
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-
 	}
 }
