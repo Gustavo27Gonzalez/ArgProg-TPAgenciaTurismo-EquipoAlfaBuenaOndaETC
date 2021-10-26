fea670efcf72ebf600aece709682be513db781b9
francoarabales <franco.arabales@gmail.com>
2021-10-26 16:56:18 -0300

    Creacion de PromocionDAO y PromocionDAOimpl

---
+++ src/main/java/dao/PromocionDAOimpl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+import promociones.Promocion;
+
+public class PromocionDAOimpl implements PromocionDAO{
+
+    public List<Promocion> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int countAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int insert(Promocion t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int update(Promocion t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int delete(Promocion t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
