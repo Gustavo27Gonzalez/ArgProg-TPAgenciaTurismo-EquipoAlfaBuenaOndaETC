843f035cb92b916ba4de613df34757a826777cdd
francoarabales <franco.arabales@gmail.com>
2021-10-27 01:30:48 -0300

    se agrego getPorsentaje en PromoPorsentual y se continuo con promocionDAOimpl, falta ToPromo, CrearPromo y AtraccionesDeLaPromo

--- src/main/java/dao/PromocionDAOimpl.java
+++ src/main/java/dao/PromocionDAOimpl.java
@@ -18,12 +18,7 @@ import tipos.Tipo;
 
 public class PromocionDAOimpl implements PromocionDAO {
 
-    //private AtraccionDAO atraccionDao;
     private Connection conn;
-
-    //public PromocionDAOimpl() {
-    //    this.atraccionDao = new AtraccionDAO();
-    //}
 
     public List<Promocion> findAll() {
         try {
@@ -57,16 +52,16 @@ public class PromocionDAOimpl implements PromocionDAO {
         return null;
     }
 
-    public int countAll(){
+    public int countAll() {
         try {
             String sql = "SELECT COUNT(1) AS TOTAL FROM Promocion";
             Connection conn = ConnectionProvider.getConnection();
             PreparedStatement statement = conn.prepareStatement(sql);
             ResultSet resultados = statement.executeQuery();
-            
+
             resultados.next();
             int total = resultados.getInt("TOTAL");
-            
+
             return total;
         } catch (SQLException ex) {
             Logger.getLogger(PromocionDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -74,12 +69,12 @@ public class PromocionDAOimpl implements PromocionDAO {
         return 0;
     }
 
-    public int insert(Promocion t){
+    public int insert(Promocion t) {
         try {
             String sql = "INSERT INTO Promocion  (nombre ,Tipo,monto,Tiempo,AtraccionGratis,Descuento) VALUES "
                     + "(?,?,?,?,?,?)";
             Connection conn = ConnectionProvider.getConnection();
-            
+
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(4, t.getNombre());
             statement.setObject(5, t.getTipo());
@@ -87,9 +82,9 @@ public class PromocionDAOimpl implements PromocionDAO {
             statement.setDouble(7, t.getDuracion());
             statement.setObject(8, ((PromoRegala) t).getRegalo());
             statement.setDouble(9, ((PromoPorcentual) t).getPorcentaje());
-            
+
             int rows = statement.executeUpdate();
-            
+
             return rows;
         } catch (SQLException ex) {
             Logger.getLogger(PromocionDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -98,16 +93,16 @@ public class PromocionDAOimpl implements PromocionDAO {
 
     }
 
-    public int update(Promocion t){
+    public int update(Promocion t) {
         try {
             String sql = "UPDATE Promocion SET Nombre = ? WHERE Tipo  = ?";
             Connection conn = ConnectionProvider.getConnection();
-            
+
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(4, t.getNombre());
             statement.setObject(5, t.getTipo());
             int rows = statement.executeUpdate();
-            
+
             return rows;
         } catch (SQLException ex) {
             Logger.getLogger(PromocionDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -116,15 +111,15 @@ public class PromocionDAOimpl implements PromocionDAO {
 
     }
 
-    public int delete(Promocion t){
+    public int delete(Promocion t) {
         try {
             String sql = "DELETE FROM Promocion WHERE Nombre LIKE ?";
             Connection conn = ConnectionProvider.getConnection();
-            
+
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(3, t.getNombre());
             int rows = statement.executeUpdate();
-            
+
             return rows;
         } catch (SQLException ex) {
             Logger.getLogger(PromocionDAOimpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -135,57 +130,19 @@ public class PromocionDAOimpl implements PromocionDAO {
 
     private Promocion toPromo(ResultSet resultados) throws Exception {
 
-        Tipo tipoAtraccion = Tipo.valueOf(resultados.getString(5));
-        Atraccion[] packAtracciones = atraccionesDeLaPromocion(resultados.getLong(2), resultados.getLong(3));
-        String nombre = resultados.getString(4);
-        String promocionTipo = resultados.getString(10);
-        Promocion promo = null;
-        promo = crearPromo(resultados, tipoAtraccion, packAtracciones, nombre, promocionTipo, promo);
-        return promo;
 
     }
 
     private Promocion crearPromo(ResultSet resultados, Tipo tipo, Atraccion[] packAtracciones,
             String nombre, String promocionTipo, Promocion promo) throws SQLException {
-        if (promocionTipo.equals("AxB")) {
-            Atraccion gratis = atraccionDao.buscarPorId(resultados.getLong(8));
-            promo = new PromoRegala(nombre, packAtracciones, tipo, gratis);
-        } else if (promocionTipo.equals("PORCENTUAL")) {
-            int descuento = resultados.getInt(9);
-            promo = new PromoPorcentual(nombre, packAtracciones, tipo, descuento);
-        } else if (promocionTipo.equals("NETO")) {
-            double monto = resultados.getInt(6);
-            promo = new PromoAbsoluta(nombre, packAtracciones, tipo, monto);
-        }
-        return promo;
+       
     }
 
     private Atraccion[] atraccionesDeLaPromocion(Long atraccion1, Long atraccion2) throws Exception {
-        Atraccion[] packs = new Atraccion[2];
-        try {
-
-            Connection conn = ConnectionProvider.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sqlAtraccion());
-            statement.setLong(1, atraccion1);
-            ResultSet result = statement.executeQuery();
-
-            while (result.next()) {
-                packs[0] = atraccionDao.toAtraccion(result);
-            }
-            statement.setLong(1, atraccion2);
-            ResultSet result2 = statement.executeQuery();
-            while (result2.next()) {
-                packs[1] = atraccionDao.toAtraccion(result2);
-            }
-            return packs;
-        } catch (Exception e) {
-            throw new Exception();
-        }
+        
     }
 
     private String sqlAtraccion() {
-        String sql = "select *" + "from Atraccion WHERE ID_Atraccion = ?";
-        return sql;
+        
     }
-
 }
